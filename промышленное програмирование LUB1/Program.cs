dfdbff184eba170c4f4e5f914a4544ad57891b66
Георгий Бурнаев <burnaev_01@mail.ru>
2022-02-07 19:01:45 +0400

    Добавьте файлы проекта.

---
+++ промышленное програмирование LUB1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace промышленное_програмирование_LUB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
